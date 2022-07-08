74648a93dabe4fa5c7899104d78fd3468b75846f
Aristides Neto <aristides@debian-BULLSEYE-live-builder-AMD64>
2022-07-08 12:40:50 -0300

    criando tabela

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/functions.php';
 
+$database = "CREATE TABLE IF NOT EXISTS `contacts` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+        `name` varchar(255) NOT NULL,
+        `email` varchar(255) NOT NULL,
+        `phone` varchar(255) NOT NULL,
+        `title` varchar(255) NOT NULL,
+        `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;";
+
+$pdo->query($database);
+
 $stmt = $pdo->query('SELECT * FROM contacts ORDER BY id');
 $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>

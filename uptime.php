670111f2073f03f5575a6d66c5c64381a005364c
Aristides Neto <aristides@debian-BULLSEYE-live-builder-AMD64>
2022-07-11 14:14:57 -0300

    add uptime

---
+++ uptime.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/functions.php';
+
+try {
+    $pdo->query('SELECT * FROM contactss');
+} catch (\Exception $e) {
+    header("HTTP/1.1 500 Internal Server Error");
+}
